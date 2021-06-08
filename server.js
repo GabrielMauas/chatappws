75cab9af4724c40131b62f2f39fcc3388fdb9823
Gabriel Mauas <gabymauas02@gmail.com>
2021-06-08 11:50:20 -0300

    subir repo

---
+++ server.js
@@ -0,0 +1,36 @@
+const { SSL_OP_NO_TICKET } = require('constants');
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User conected, host: ', socket.handshake.headers.host);
+    socket.on('disconnect', () => {
+        console.log('User disconnected, id: ', socket.handshake.headers.host);
+    });
+});
+
+io.on('connection', socket => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', {mens: msg, data: socket.id});
+    });
+})
+
+server.listen(4000, () => {
+    console.log('Server is running...');
+});
+
+
+
+
+
+
+
+
